3e21d728c9c74650c14c156083b2dbe608cf1a19
Elvis <elvishenry2402@gmail.com>
2021-10-22 14:32:31 +0200

    Add first button switcher img

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,1 +1,12 @@
 //alert("Toujours en cours de création..Veuillez patienter :)");
+
+var indexImg = 0;
+
+let img = document.querySelector("#Img");
+let btn = document.querySelector("#Next");
+
+btn.addEventListener('click', () => 
+{
+    img.src = "Images/Games Images/Unity/The Guardian/Image"+ indexImg +".PNG";
+    indexImg < 5 ? indexImg++ : indexImg = 0;
+})
