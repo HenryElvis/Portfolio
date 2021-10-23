43e8efd45f910bc38106accbd09be56269f92798
Elvis <elvishenry2402@gmail.com>
2021-10-23 15:54:00 +0200

    Add Follow Light image

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,13 +1,25 @@
 //alert("Toujours en cours de création..Veuillez patienter :)");
 
-var indexImg = 0;
-var imgSrc = "Images/Games Images/Unity/";
+var indexGuardian = 0;
+var imgSrc = "Images/Games Images/";
 
-let img = document.querySelector("#Img");
+let img = document.getElementById("Img");
+let imgFollowLight = document.querySelector("#ImgFollowLight");
+
 let btn = document.querySelector("#Next");
+let btnNextFollowLight = document.querySelector("#NextFollowLight");
 
-btn.addEventListener('click', () => 
+function ChangeImgAtButtonPressed(nameOfFolder, numberFile)
 {
-    indexImg < 5 ? indexImg++ : indexImg = 0;
-    img.src = imgSrc + "The Guardian/Image"+ indexImg +".PNG";
-})
+    indexGuardian < numberFile ? indexGuardian++ : indexGuardian = 0;
+    img.src = imgSrc + nameOfFolder + indexGuardian + ".JPG";
+}
+
+function ChangeImgFollowLightAtButtonPressed(nameOfFolder, numberFile)
+{
+    indexGuardian < numberFile ? indexGuardian++ : indexGuardian = 0;
+    imgFollowLight.src = imgSrc + nameOfFolder + indexGuardian + ".JPG";
+}
+
+btn.addEventListener('click', () => ChangeImgAtButtonPressed("Unity/The Guardian/Image", 5, img.src))
+btnNextFollowLight.addEventListener('click', () => ChangeImgFollowLightAtButtonPressed("Unreal engine/Follow The Light/Image", 5))
