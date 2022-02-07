84f8c4a8106de1cd317df212253e9ef607ba3350
Elvis <elvishenry2402@gmail.com>
2022-02-08 00:18:21 +0100

    Add new file

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,18 +1,27 @@
 //alert("Toujours en cours de création..Veuillez patienter :)");
 
+var imgSrc = "Images/Games Images/";
+
 var indexGuardian = 0;
 var indexFollowLight = 0;
 var indexArmy = 0;
-
-var imgSrc = "Images/Games Images/";
+var indexNin = 0;
+var indexBe = 0;
+var indexAdv = 0;
 
 let img = document.getElementById("Img");
 let imgFollowLight = document.getElementById("ImgFollowLight");
 let imgArmy = document.getElementById("ImgArmy");
+let imgNin = document.getElementById("ImgNin");
+let imgBe = document.getElementById("ImgBe");
+let imgAdv = document.getElementById("ImgAdv");
 
 let btn = document.querySelector("#Next");
 let btnNextFollowLight = document.querySelector("#NextFollowLight");
 let btnArmy = document.querySelector("#NextArmy");
+let btnNin = document.querySelector("#NextNinja");
+let btnBe = document.querySelector("#NextBe");
+let btnAdv = document.querySelector("#NextAdv");
 
 function ChangeImgAtButtonPressed(nameOfFolder, numberFile)
 {
@@ -32,9 +41,34 @@ function ChangeImgArmyAtButtonPressed(nameOfFolder, numberFile)
     imgArmy.src = imgSrc + nameOfFolder + indexArmy + ".JPG";
 }
 
+function ChangeImgNinAtButtonPressed(nameOfFolder, numberFile)
+{
+    indexNin < numberFile ? indexNin++ : indexNin = 0;
+    imgNin.src = imgSrc + nameOfFolder + indexNin + ".JPG";
+}
+
+function ChangeImgBeAtButtonPressed(nameOfFolder, numberFile)
+{
+    indexBe < numberFile ? indexBe++ : indexBe = 0;
+    imgBe.src = imgSrc + nameOfFolder + indexBe + ".JPG";
+}
+
+function ChangeImgAdvAtButtonPressed(nameOfFolder, numberFile)
+{
+    indexAdv < numberFile ? indexAdv++ : indexAdv = 0;
+    imgAdv.src = imgSrc + nameOfFolder + indexAdv + ".JPG";
+}
+
 btn.addEventListener('click', () => ChangeImgAtButtonPressed("Unity/The Guardian/Image", 5))
 
 btnNextFollowLight.addEventListener('click', 
 () => ChangeImgFollowLightAtButtonPressed("Unreal Engine/Follow The Light/Image", 5))
 
 btnArmy.addEventListener('click', () => ChangeImgArmyAtButtonPressed("Unreal Engine/Army/Image", 6))
+
+btnNin.addEventListener('click', () => ChangeImgNinAtButtonPressed("Unreal Engine/Ninja's Adventure/Image", 14))
+
+btnBe.addEventListener('click', () => ChangeImgBeAtButtonPressed("Unreal Engine/Be Stealthy/Image", 9))
+
+btnAdv.addEventListener('click', () => ChangeImgAdvAtButtonPressed("Unreal Engine/Adventurer/Image", 16))
+
